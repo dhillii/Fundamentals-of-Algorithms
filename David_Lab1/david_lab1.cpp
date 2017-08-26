d9df39a952136d3287cc9c2d32a24a2fea3d41c2
David Hill <davidhill@Davids-MacBook-Air.local>
2017-08-25 23:51:33 -0400

    Added comments 📝

--- David_Lab1/david_lab1.cpp
+++ David_Lab1/david_lab1.cpp
@@ -14,13 +14,13 @@ Due: 8/28/2017
 
 using namespace std;
 
-void bubbleSort(int Arr[]);							
+void bubbleSort(int Arr[], int size);							
 void bubbleSort_str(vector<string> &strArr);
 
 
-void bubbleSort(int Arr[], int size)
+void bubbleSort(int Arr[], int size)	// "int size" is the Initialization step		
 {
-    for (int x = 0; x < size; x++)
+    for (int x = 0; x < size; x++)		// Maintainence steps
     {
         for (int y = 0; y < size - x - 1; y++)
         {
@@ -30,21 +30,21 @@ void bubbleSort(int Arr[], int size)
             }
         }
         
-    }
+    }									// End of maintainence steps
 
 }
 
 void bubbleSort(vector<string> &strArr)
 {
-    int size = strArr.size();
+    int size = strArr.size();			// Initilization step of loop invariant.
 
     
-    for (int x = 0; x < size; x++)
+    for (int x = 0; x < size; x++)		// Maintainence steps 
     {
         for (int y = 0; y < size - x - 1; y++)
         {
-            int L1 = int(tolower(strArr[y][0]));
-            int L2 = int(tolower(strArr[y+1][0]));
+            int L1 = int(tolower(strArr[y][0]));		// store ascii values of first letter in each string.
+            int L2 = int(tolower(strArr[y+1][0]));		//  lower cased to allow for proper sorting of uppercased and lower cased strings
             
             if (L1 > L2)
             {
@@ -53,7 +53,7 @@ void bubbleSort(vector<string> &strArr)
             }
         }
         
-    }
+    }									// End of maintainence steps
 }
 
 
@@ -68,18 +68,18 @@ int main()
 
 	cout << " ";
 
-	for (int i = 0; i < 11; i++)
+	for (int i = 0; i < 11; i++)	// Termination steps
 	{
 		cout << A[i] << " ";
 	}
 
 	cout << endl;
 
-	bubbleSort(A, 11);
+	bubbleSort(A, 11);				
 
 	cout << " ";
 
-	for (int i = 0; i < 11; i++)
+	for (int i = 0; i < 11; i++)	// End of termination steps
 	{
 		cout << A[i] << " ";
 	}
@@ -104,19 +104,19 @@ int main()
 
     cout << " Unsorted: " ;
     
-    for(int idx = 0; idx < names; idx++)
+    for(int idx = 0; idx < names; idx++)		// Termination steps
     {
         cout << " " << B[idx] << " ";
     }
     
 
-    bubbleSort(B);
+    bubbleSort(B); 								
     
     cout << endl;
 
     cout << " Sorted: ";
     
-    for(int idx = 0; idx < names; idx++)
+    for(int idx = 0; idx < names; idx++)		// End of termination steps
     {
         cout << " " << B[idx] << " ";
     }
